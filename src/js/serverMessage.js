3d0ed434a36e32afa9d1155b968d75c2f9f3aef6
lycanea <lycanea@lycanea.dev>
2025-03-23 17:36:49 +1100

    Add a server message thingy that pulls a status message from the lycanea api (https://api.lycanea.dev/modded/message)

---
+++ src/js/serverMessage.js
@@ -0,0 +1,25 @@
+const apiBase = "https://api.lycanea.dev";
+const apiUrl = `${apiBase}/modded/message`;
+
+function fetchServerMessage() {
+	fetch(apiUrl)
+		.then((response) => {
+			if (!response.ok) {
+				throw new Error("http response was not ok");
+			}
+			return response.json();
+		})
+		.then((data) => {
+			updateServerMessage(data);
+		})
+		.catch((error) => {
+			console.error("Error fetching server message: ", error);
+		});
+}
+
+function updateServerMessage(data) {
+	document.getElementById("server-custom-status").textContent = data;
+}
+
+fetchServerMessage();
+setInterval(fetchServerMessage, 10000);
